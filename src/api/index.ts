7aa3ca1d66016084415088f019e491046745610a
miguel174 <miguel@tinkin.one>
2023-05-25 08:31:11 -0500

    new base url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://d95a-45-230-240-152.ngrok-free.app',
+  baseURL: 'https://dce5-45-230-240-147.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json'
   }

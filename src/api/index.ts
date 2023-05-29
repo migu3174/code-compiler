3789eb697adf2c452c8bd72e5343fb7457738ebe
miguel174 <miguel@tinkin.one>
2023-05-29 12:43:51 -0500

    fix env import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-console.log('process.env.BASE_URL', process.env.BASE_URL)
-
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'https://0774-45-230-240-147.ngrok-free.app',
+  baseURL: import.meta.env.VITE_BASE_URL || 'https://0774-45-230-240-147.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json'
   }

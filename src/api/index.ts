819016e45958fe440d7d16ca3ee5b0f76cfa90f4
miguel174 <miguel@tinkin.one>
2023-05-29 12:30:29 -0500

    test

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 console.log('process.env.BASE_URL', process.env.BASE_URL)
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.BASE_URL || 'https://0774-45-230-240-147.ngrok-free.app',
   headers: {
     'Content-Type': 'application/json'
   }
